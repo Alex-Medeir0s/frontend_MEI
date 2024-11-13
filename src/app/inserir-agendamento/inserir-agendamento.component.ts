9224bdd53668ca1f05cafc14bdd546026c6d78c8
Alex-Medeir0s <alexmedeiros157@outlook.com>
2024-11-13 20:33:29 -0300

    Add address section to appointment form and implement CEP lookup

--- src/app/inserir-agendamento/inserir-agendamento.component.ts
+++ src/app/inserir-agendamento/inserir-agendamento.component.ts
@@ -2,35 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Agendamento } from '../model/agendamento';
+import { Endereco } from '../model/endereco';
 import { AgendamentoService } from '../services/agendamento.service';
+import { ViaCepService } from '../services/viacep/via-cep.service';
 
 @Component({
   selector: 'app-inserir-agenda',
   templateUrl: './inserir-agendamento.component.html',
   styleUrls: ['./inserir-agendamento.component.css']
 })
-export class InserirAgendamentoComponent {
+export class InserirAgendamentoComponent implements OnInit {
 
   codigo!: number;
   agendamento: Agendamento = new Agendamento();
   
-  constructor(private AgendamentoService: AgendamentoService, private router: Router) { }
+  constructor(
+    private agendamentoService: AgendamentoService, 
+    private router: Router, 
+    private viaCepService: ViaCepService
+  ) {}
 
   ngOnInit(): void {
+    this.agendamento.endereco = new Endereco();
+  }
 
+  buscarCep() {
+    const cep = this.agendamento.endereco.cep.replace(/\D/g, ''); // Remove caracteres não numéricos
+    if (cep.length === 8) {
+      this.viaCepService.buscarCep(cep).subscribe(
+        (data) => {
+          if (data.erro) {
+            alert('CEP não encontrado.');
+          } else {
+            this.agendamento.endereco.logradouro = data.logradouro;
+            this.agendamento.endereco.cidade = data.localidade;
+            this.agendamento.endereco.uf = data.uf;
+            this.agendamento.endereco.bairro = data.bairro;
+          }
+        },
+        (error) => {
+          console.error('Erro ao buscar CEP:', error);
+        }
+      );
+    }
   }
 
   retornar() {
     this.router.navigate(['listar-agendas']);
   }
 
-  onSubmit(){
+  onSubmit() {
     this.agendamento.codigo = 0;
-    this.AgendamentoService.incluirAgendamento(this.agendamento).subscribe( data => {
-      console.log(data);
-      this.retornar();
-    });
+    this.agendamentoService.incluirAgendamento(this.agendamento).subscribe(
+      (data) => {
+        console.log(data);
+        this.retornar();
+      }
+    );
   }
-
-
 }
